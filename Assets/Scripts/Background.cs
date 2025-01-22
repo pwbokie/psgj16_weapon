0b22b22704b86fbe52ba0bd754c714e0b69a54fa
Paul <pmerkamp@gmail.com>
2025-01-21 21:54:24 -0500

    nice background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public static float driftSpeedX = 0.1f;
+    public static float driftSpeedY = 0.2f;
+    private static Vector2 drift = new Vector2(driftSpeedX, driftSpeedY);
+
+    public int tilesWidth = 6;
+    public int tilesHeight = 4;
+
+    public GameObject cam;
+
+    public GameObject tilePrefab;
+
+    public Sprite sprite;
+    public Color foregroundColor;
+
+    public void Start() {
+        // Spawn tiles to fill the visible background
+        float spriteWidth = tilePrefab.GetComponent<SpriteRenderer>().bounds.size.x;
+        float spriteHeight = tilePrefab.GetComponent<SpriteRenderer>().bounds.size.y;
+
+        for (int i=-(tilesWidth/2); i < tilesWidth/2; i++) {
+            for (int j=-(tilesHeight/2); j < tilesHeight/2; j++) {
+                GameObject tile = Instantiate(tilePrefab, new Vector2(spriteWidth * i, spriteHeight * j), Quaternion.identity);
+                tile.transform.parent = transform;
+                tile.GetComponent<SpriteRenderer>().sprite = sprite;
+                tile.GetComponent<SpriteRenderer>().color = foregroundColor;
+            }
+        }
+
+        drift = new Vector2(driftSpeedX, driftSpeedY);
+    }
+
+    private Vector2 currentOffset;
+
+    public void Update() {
+        Vector2 newPosition = (Vector2)transform.position + drift * Time.deltaTime;
+
+        // Define bounds relative to the camera
+        float xMin = cam.transform.position.x - 10;
+        float xMax = cam.transform.position.x + 10;
+        float yMin = cam.transform.position.y - 5;
+        float yMax = cam.transform.position.y + 5;
+
+        // Wrap the X-axis position seamlessly
+        if (newPosition.x > xMax) {
+            newPosition.x = xMin + (newPosition.x - xMax);
+        } else if (newPosition.x < xMin) {
+            newPosition.x = xMax - (xMin - newPosition.x);
+        }
+
+        // Wrap the Y-axis position seamlessly
+        if (newPosition.y > yMax) {
+            newPosition.y = yMin + (newPosition.y - yMax);
+        } else if (newPosition.y < yMin) {
+            newPosition.y = yMax - (yMin - newPosition.y);
+        }
+
+        // Apply the new position
+        transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
+    }
+}
