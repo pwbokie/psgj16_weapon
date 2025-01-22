4ffc4228bbf4bae44b3e07eb8d04c109106c42c4
rmgraham45 <32076943+rmgraham45@users.noreply.github.com>
2025-01-21 20:51:36 -0500

    adding map prefabs

--- Assets/Scripts/Map/MapGenerator.cs
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -18,7 +18,7 @@ public class MapGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GenerateMap();
+        
     }
 
     // Update is called once per frame
@@ -27,42 +27,4 @@ public class MapGenerator : MonoBehaviour
         
     }
 
-    void GenerateMap()
-    {
-        map = new int[width, height];
-    }
-
-    void RandomFillMap()
-    {
-        if(useRandomSeed)
-        {
-            seed = Time.time.ToString();
-        }
-
-        System.Random rand = new System.Random(seed.GetHashCode());
-
-        for(int x = 0; x < width; x++)
-        {
-            for(int y = 0; x < height; y++)
-            {
-                map[x,y] = (rand.Next(0,100) < randomFillPercent) ? 1 : 0;
-            }
-        }
-    } 
-
-    void OnDrawGizmos()
-    {
-        if (map != null)
-        {
-            for(int x= 0; x < width; x++) 
-            {
-                for(int y = 0; y < height; y++)
-                {
-                    Gizmos.color = (map[x,y] == 1) ? Color.black : Color.white;
-                    Vector3 pos = new Vector3(-width/2 + x + .5f, -height/2 + y+.5f, 0);
-                    Gizmos.DrawCube(pos, Vector3.one);
-                }
-            }
-        }
-    }
 }
