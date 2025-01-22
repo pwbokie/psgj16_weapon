53f58ba068af1a6741caf206883f1fe161cf7bf6
Paul <pmerkamp@gmail.com>
2025-01-22 13:54:09 -0500

    give bubbles movement options and make them better

--- Assets/Scripts/Object/ItemBubble.cs
+++ Assets/Scripts/Object/ItemBubble.cs
@@ -2,13 +2,20 @@ using UnityEngine;
 
 public class ItemBubble : MonoBehaviour
 {
-    public float contentsRotationSpeed = 1f;
     public GameObject contents;
+    public ItemBubbleMovementType movementType = ItemBubbleMovementType.STATIC;
+    
+    private float floatyPushFrequency = 5f;
+
+    private Rigidbody2D rb2d;
+
     private GameObject crate;
     private Rigidbody2D contentsRb2d;
 
     public void Start()
     {    
+        rb2d = GetComponent<Rigidbody2D>();
+
         crate = transform.GetChild(0).gameObject;
         crate.GetComponent<LootCrate>().SetContents(contents);
         contentsRb2d = crate.GetComponent<Rigidbody2D>();
@@ -16,21 +23,30 @@ public class ItemBubble : MonoBehaviour
 
     public void Update()
     {
-        crate.transform.Rotate(0, 0, contentsRotationSpeed);
-    }
-
-    public void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
+        // If the movement type is FLOATY, apply a gentle force to the bubble every floatyPushPushFrequency seconds
+        if (movementType == ItemBubbleMovementType.FLOATY)
         {
-            Pop();
+            if (Time.time % floatyPushFrequency < 0.01f)
+            {
+                rb2d.AddForce(new Vector2(Random.Range(-100f, 100f), Random.Range(-100f, 100f)));
+            }
+
+            // dampen movement
+            rb2d.velocity *= 0.999f;
         }
     }
 
     public void Pop() {
         crate.transform.parent = null;
-        contentsRb2d.bodyType = RigidbodyType2D.Dynamic;
+        contentsRb2d.simulated = true;
 
         gameObject.GetComponent<ShadowedObject>().DestroyThisAndItsShadow();
     }
+}
+
+public enum ItemBubbleMovementType
+{
+    STATIC,
+    FLOATY,
+    GRAVITY
 }
