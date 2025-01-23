089274794ea924666ff6c755b10f37ae95770d7e
rmgraham45 <32076943+rmgraham45@users.noreply.github.com>
2025-01-23 14:19:40 -0500

    added laser image

--- Assets/Scripts/Enemies/Turret.cs
+++ Assets/Scripts/Enemies/Turret.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Turret : MonoBehaviour
@@ -7,8 +8,9 @@ public class Turret : MonoBehaviour
     private Rigidbody2D turret;
     private float rotationDamping = 2f;
     private float torqueForce = 1f;
+    public bool debug = false;
 
-    private float maxRotationSpeed = 0.3f;
+    private float maxRotationSpeed = 30f;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,15 +32,20 @@ public class Turret : MonoBehaviour
         float currentAngle = turret.rotation;
 
         float angleDifference = Mathf.DeltaAngle(currentAngle, targetAngle);
+
         if(Mathf.Abs(angleDifference) > 1f)
         {
             float torque = angleDifference * torqueForce;
 
-            //Debug.Log(torque * rotationDamping);
             if(torque * rotationDamping > maxRotationSpeed)
+            {
                 turret.AddTorque(maxRotationSpeed);
+            }         
             else
+            {
                 turret.AddTorque(torque * rotationDamping);
+            }
+                
         }
         else
         {
---
+++ Assets/Scripts/FX/Laser.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
