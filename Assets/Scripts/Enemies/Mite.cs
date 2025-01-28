ad26d6b9d017e3b917f634b61c027273f3c81732
rmgraham45 <32076943+rmgraham45@users.noreply.github.com>
2025-01-27 20:39:27 -0500

    thing

--- Assets/Scripts/Enemies/Mite.cs
+++ Assets/Scripts/Enemies/Mite.cs
@@ -1,10 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Mite : Enemy
 {
     private Rigidbody2D mite;
+    private float visionDistance = 0.085f;
+    private float moveSpeed = 2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,24 @@ public class Mite : Enemy
     // Update is called once per frame
     void Update()
     {
+        Vector3 playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        Vector3 playerWorldPosition = Camera.main.ScreenToWorldPoint(playerPosition);
+
+        
+        Vector3 mitePosition = Camera.main.ScreenToWorldPoint(mite.transform.position);
+
+        Debug.Log(Vector3.Distance(mitePosition, playerWorldPosition) + "\nVision: " + visionDistance);
+        if(Vector2.Distance(mitePosition, playerWorldPosition) < visionDistance)
+        {
+            Debug.Log("move");
+            Vector2 direction = (playerWorldPosition - mitePosition).normalized;
+
+            if(direction.x > 0)
+                mite.velocity = new Vector2(1 * moveSpeed, 0);
+            else
+                mite.velocity = new Vector2(-1 * moveSpeed, 0);
+
+        }
         UpdateEnemy();
     }
 }
